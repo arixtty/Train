bed1845d7181c189a0124fb71a23e6ddfb6ec159
arixtty <arixtty@gmail.com>
2019-07-06 22:41:37 +0300

    Events fixes

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var toggleButton = document.querySelector(".header__nav-button");
 var mobileNav = document.querySelector(".header__mobile-nav");
-var anyBody = document.querySelector("body");
+var anyBody = document.querySelector("main");
 
 
 toggleButton.addEventListener("click", function() {
@@ -10,17 +10,16 @@ toggleButton.addEventListener("click", function() {
 		}, 10);
 });
 
+anyBody.addEventListener("touchend", function() {
+  mobileNav.classList.remove("open");
+	setTimeout(function() {  
+		  mobileNav.classList.remove("open");
+		}, 20);
+});
 
 anyBody.addEventListener("click", function() {
   mobileNav.classList.remove("open");
 	setTimeout(function() {  
 		  mobileNav.classList.remove("open");
-		}, 5);
-});
-
-anyBody.addEventListener("touchstart", function() {
-  mobileNav.classList.remove("open");
-	setTimeout(function() {  
-		  mobileNav.classList.remove("open");
-		}, 5);
+		}, 15);
 });
